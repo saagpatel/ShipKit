09702bfe7510f8df36218c25771d0ecce8ae5683
Saagar <saagarpatelai@gmail.com>
2026-07-03 07:31:14 -0700

    test: cover migration checksum hex output

--- packages/core/src/db/migration.rs
+++ packages/core/src/db/migration.rs
@@ -316,6 +316,15 @@ mod tests {
     }
 
     #[test]
+    fn checksum_outputs_lowercase_hex_digest() {
+        let checksum = MigrationEngine::checksum("CREATE TABLE test (id INTEGER);");
+
+        assert_eq!(checksum.len(), 64);
+        assert!(checksum.chars().all(|ch| ch.is_ascii_hexdigit()));
+        assert_eq!(checksum, checksum.to_ascii_lowercase());
+    }
+
+    #[test]
     fn apply_single_migration() {
         let pool = test_pool();
         let mut engine = MigrationEngine::new(pool.clone());
